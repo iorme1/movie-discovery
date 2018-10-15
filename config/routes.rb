becde0159a84646cc2f5f101b1790c681cac4277
Isaac Orme <isorme1@gmail.com>
2018-10-15 14:52:59 -0700

    Configures routes for tvshows index/show, Adds basic view for index and search partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'tvshows/index'
-  get 'tvshows/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tvshows, only: [:index, :show]
+  root 'tvshows#index'
 end
